3c97da9b4e3f9294f52c86438873459e346112d8
khurley <khurley@khurley-HP>
2013-11-26 01:57:08 -0300

    removing wrong configuration

--- quiz-services/src/test/java/com/khurley/quiz/module/ServicesTestContext.java
+++ quiz-services/src/test/java/com/khurley/quiz/module/ServicesTestContext.java
@@ -3,14 +3,12 @@ package com.khurley.quiz.module;
 import static org.mockito.Mockito.mock;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.khurley.quiz.dao.QuizRepository;
 
 @Configuration
-@ComponentScan("com.khurley.quiz.module.services")
 @Import(ServicesContext.class)
 public class ServicesTestContext {
 
